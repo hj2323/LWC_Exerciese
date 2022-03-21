00762041959f3f6911e7eaf3c60e667d7ea2443b
Hyeji Choi <hjchoi2323@gmail.com>
2022-03-21 16:08:05 +0900

    Exercise 2-5 Completed

---
+++ force-app/Exercises/main/default/lwc/studentTile/studentTile.js
@@ -0,0 +1,18 @@
+import { LightningElement, api } from 'lwc';
+
+export default class StudentTile extends LightningElement {
+    @api student = {
+        Name: '최혜지',
+        PhotoUrl: '/services/images/photo/003B0FakePictId',
+        };
+
+    @api isSelected = false;
+
+    get tileSelected() {
+        return this.isSelected ? "tile selected" : "tile";
+    }
+
+    studentClick(){
+        alert(this.student.Name);
+    }
+}
