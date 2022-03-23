fbfc3d2dd7a35d03fe7014a7fb1ff4d38a58d3fb
Hyeji Choi <hjchoi2323@gmail.com>
2022-03-23 15:28:09 +0900

    Exercise 4-3 Completed

--- force-app/Exercises/main/default/lwc/certifiedStudentList/certifiedStudentList.js
+++ force-app/Exercises/main/default/lwc/certifiedStudentList/certifiedStudentList.js
@@ -1,5 +1,8 @@
 import { LightningElement, api, wire } from 'lwc';
 import getCertifiedStudents from '@salesforce/apex/CertifiedStudentList.getCertifiedStudents';
+import deleteStudentCertification from '@salesforce/apex/CertifiedStudentList.deleteStudentCertification';
+import { refreshApex } from '@salesforce/apex';
+
 
 export default class CertifiedStudentList extends LightningElement {
     @api certificationId = 0;
@@ -7,9 +10,11 @@ export default class CertifiedStudentList extends LightningElement {
     certifiedStudents;
     btnGroupDisabled = true;
     error;
+    _wiredStudentResult;
 
     @wire(getCertifiedStudents, {certificationId:'$certificationId'})
     wired_getCertifiedStudents(result) {
+        this._wiredStudentResult = result;
         this.certifiedStudents = [];
         if (result.data) {
             this.certifiedStudents = result.data.map(certHeld => ({
@@ -27,6 +32,17 @@ export default class CertifiedStudentList extends LightningElement {
         } else if (result.error) {
         this.error = result.error;
         }
+    }
+
+    onDelete() {
+        const certificationIds = this.getSelectedIDs();
+        deleteStudentCertification({certificationIds})
+        .then( () => {
+            refreshApex(this._wiredStudentResult);
+        })
+        .catch(error => {
+            this.error = error;
+        });
     }
 
     //data table column 이름 값 주기 
@@ -58,4 +74,30 @@ export default class CertifiedStudentList extends LightningElement {
         const numSelected = event.detail.selectedRows.length;
         this.btnGroupDisabled = (numSelected === 0);
     }
+
+    getSelectedIDs() {
+        const datatable = this.template.querySelector('lightning-datatable');
+        const ids = datatable.getSelectedRows().map( (r) => (
+            r.certificationHeldId
+        ));
+        return ids;
+    }
+
+    onCertActions (event) {
+        const btnClicked = event.target.getAttribute('data-btn-id');
+        switch (btnClicked) {
+            case 'btnEmail':
+            break;
+            case 'btnSendCert':
+            break;
+            case 'btnDelete':
+            this.onDelete();
+            break;
+            default:
+            break;
+            }
+    }
+
+    
+
 }
